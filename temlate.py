a8ca6c28719855f12ad2dc6ccc3514f7f51ba1e8
aslibaraf <himalayparmar6.com>
2024-07-09 20:43:42 +0530

    notebook data added

--- temlate.py
+++ temlate.py
@@ -1,12 +1,12 @@
 import os
 from pathlib import Path
-project_name = 'Backorder_Prediction'
 
-listoffile= [
+project_name = 'Your_Project_Name'
 
+files_to_create = [
     f"{project_name}/__init__.py",
     f"{project_name}/components/__init__.py",
-    f"{project_name}/components/data_ingestion.py",  
+    f"{project_name}/components/data_ingestion.py",
     f"{project_name}/components/data_validation.py",
     f"{project_name}/components/data_transformation.py",
     f"{project_name}/components/model_trainer.py",
@@ -19,9 +19,9 @@ listoffile= [
     f"{project_name}/entity/artifact_entity.py",
     f"{project_name}/exception/__init__.py",
     f"{project_name}/logger/__init__.py",
-    f"{project_name}/pipline/__init__.py",
-    f"{project_name}/pipline/training_pipeline.py",
-    f"{project_name}/pipline/prediction_pipeline.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/main_utils.py",
     "app.py",
@@ -34,14 +34,13 @@ listoffile= [
     "config/schema.yaml",
 ]
 
-
-for filepath in listoffile:
-    filepath = Path(filepath)
-    filedir, filename = os.path.split(filepath)
-    if filedir != "":
-        os.makedirs(filedir, exist_ok=True)
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, "w") as f:
-            pass
+for file_path in files_to_create:
+    file_path = Path(file_path)
+    file_directory = file_path.parent
+    print('this is file direct',file_directory)
+    if not file_directory.exists():
+        file_directory.mkdir(parents=True, exist_ok=True)
+    if not file_path.exists() or file_path.stat().st_size == 0:
+        file_path.touch()
     else:
-        print(f"file is already present at: {filepath}")
+        print(f"File already exists at: {file_path}")
